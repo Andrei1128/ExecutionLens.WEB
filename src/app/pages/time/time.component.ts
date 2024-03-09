9bd164264f6e2f79f2f6da20980f74cbfd453177
Andrei <nicolaev.andrei09@gmail.com>
2024-03-09 14:07:35 +0200

    added responsivnes + char js

--- src/app/pages/time/time.component.ts
+++ src/app/pages/time/time.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-time',
   standalone: true,
   imports: [],
   templateUrl: './time.component.html',
-  styleUrl: './time.component.scss'
+  styleUrl: './time.component.scss',
 })
-export class TimeComponent {
+export class TimeComponent implements OnInit {
+  chart: Chart | null = null;
 
+  ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {},
+    });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (this.chart) {
+      this.chart.resize();
+    }
+  }
 }
--- src/app/pages/time/time.module.ts
+++ src/app/pages/time/time.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TimeRoutingModule } from './time-routing.module';
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    TimeRoutingModule
-  ]
+  imports: [CommonModule, TimeRoutingModule],
 })
-export class TimeModule { }
+export class TimeModule {}
