b4eadf72e2950803d79ec967ef2d7b0dd24b20d4
Andrei-Ionuţ Nicolaev <andrei.nicolaev@365.univ-ovidius.ro>
2024-03-21 20:56:24 +0200

    wip search page

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedModule } from '../../_core/modules/shared.module';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import Chart from 'chart.js/auto';
+import {
+  FormGroup,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatRadioModule,
+  ],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  requestsCountChart: Chart | null = null;
 
+  filters = new FormGroup({
+    dateStart: new FormControl<Date | null>(null),
+    dateEnd: new FormControl<Date | null>(null),
+    controllers: new FormControl(),
+    endpoints: new FormControl(),
+  });
+
+  ngOnInit() {}
+
+  fetch() {}
+
+  controllerList: string[] = [
+    'UserController',
+    'ProductController',
+    'OrderController',
+    'PaymentController',
+    'CartController',
+  ];
+
+  orderOptions: any[] = [
+    { value: 0, viewValue: 'ASC' },
+    { value: 1, viewValue: 'DESC' },
+  ];
+
+  endpointList: string[] = ['user', 'product', 'order', 'payment', 'cart'];
 }
